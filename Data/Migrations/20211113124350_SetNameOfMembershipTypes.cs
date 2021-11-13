4a774a820c410643a2bcb11750cc00a37373924f
Leśniak Tomasz <tomasz.lesniak@microsoft.wsei.edu.pl>
2021-11-13 13:52:16 +0100

    Migrations

---
+++ Data/Migrations/20211113124350_SetNameOfMembershipTypes.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibApp.Data.Migrations
+{
+    public partial class SetNameOfMembershipTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE MembershipTypes set Name = 'Pay as You go' where Id = 1");
+            migrationBuilder.Sql("UPDATE MembershipTypes set Name = 'Monthly' where Id = 2");
+            migrationBuilder.Sql("UPDATE MembershipTypes set Name = 'Quarterly' where Id = 3");
+            migrationBuilder.Sql("UPDATE MembershipTypes set Name = 'Yearly' where Id = 4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
